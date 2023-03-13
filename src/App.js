f2f4d5ae617ada96d08d0ea877cb48f6978f55d3
Beth C <99213968+bethsaysboo@users.noreply.github.com>
2023-03-13 15:42:26 -0500

    Updated App file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import React from "react";
+import "./styles.css";
+import Header from "./Components/Header";
+import { localNpc } from "./Components/NonPlayableChar";
+import NonPlayableChar from "./Components/NonPlayableChar";
+import PersonDetail from "./Components/NonPlayableChar";
+import Instructions from "./Components/Instructions";
+import image from "./image/Stardew.png";
 
 function App() {
+  const [isShown, setIsShown] = useState(false);
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("The link was clicked.");
+    setIsShown((current) => !current);
+  };
+
+  // return <p> Stardew Singles!</p>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body" style={{ backgroundImage: `url(${image})` }}>
+      <>
+        <Header />
+        <Instructions />
+        <main className="main">
+          {/* <aside> */}
+          {localNpc.map((data) => {
+            return (
+              <NonPlayableChar
+                pictures={data.pictures}
+                nameNpc={data.nameNpc}
+                handleClick={handleClick}
+                loves={data.loves}
+                hates={data.hates}
+                description={data.description}
+              />
+            );
+          })}
+          {/* </aside> */}
+          {/* {localNpc.map((data) => {
+          return (
+            <PersonDetail
+              pictures={data.pictures}
+              nameNpc={data.nameNpc}
+              handleClick={handleClick}
+              loves={data.loves}
+              hates={data.hates}
+              description={data.description}
+            />
+          );
+        })}
+        ; */}
+        </main>
+      </>
     </div>
   );
 }
 
 export default App;
+
