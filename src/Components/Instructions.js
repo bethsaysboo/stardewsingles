44fbf0e07403ea24ed943f0d50f49c2b6a5b740f
bethsaysboo <bethcaswell98@gmail.com>
2023-09-01 11:27:52 -0500

    marriage typo fix

--- src/Components/Instructions.js
+++ src/Components/Instructions.js
@@ -1,6 +1,6 @@
 function Instructions (){
     return( <div className="instructions">
-    <h2> Want to know how to woo your favorite NPC into marrige?<br></br> Click on the images below to learn about each NPC!</h2>
+    <h2> Want to know how to woo your favorite NPC into marriage?<br></br> Click on the images below to learn about each NPC!</h2>
 </div>);
     
 }
